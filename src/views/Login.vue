87edcb7363a952d5c4c095f0a17d8fa705cfc446
suresh roy <sureshroy709@gmail.com>
2022-01-08 01:26:41 +0530

    ng

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,20 +2,28 @@
   <div class="loginPage">
     <div class="login">
       <h1>Login</h1>
-      <form @submit.prevent="login">
+
+      <form class="input" @submit.prevent="login">
+          <div>
         Enter Your E-mail :
         <input
           type="email"
           placeholder="Your email"
           v-model="email"
-        /><br /><br />
+        />
+          </div>
+          <div>
         Enter Your Password :
         <input
           type="password"
           placeholder="Your password"
           v-model="password"
-        /><br /><br />
-        <input type="submit" value="Login" />
+        />
+          </div>
+          <div class="button">
+              <input  type="submit" value="Login" />
+          </div>
+        
       </form>
       <p>Need an account? <router-link to="/register">Register</router-link></p>
     </div>
@@ -54,13 +62,58 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  background: url("../assets/login_bg.jpg");
+  background-size: cover;
 }
-.login{
+.login {
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  width: 400px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.250);
+  justify-content: space-evenly;
+  align-items: center;
+  width: 500px;
+  height: 400px;
+  border-radius: 40px;
+  /* color: black; */
+  background-color: rgba(0, 0, 0, 0.479);
+}
+.input {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 100px;
+}
+.button input{
+    outline: none;
+  /* margin-right: 15px; */
+  padding: 9px 17px 9px 17px;
+  font-size: 20px;
+  background-color: #ef4136;
+  border-radius: 8px;
+  margin-top:10px;
+  color: white;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+  ;
+}
+.button input:hover{
+    background-color: white;
+    color:#ef4136 ;
+}
+input{
+    font-family: sans-serif;
+    outline: none;
+    border-style:none;
+    padding: 5px;
+    margin: 8px;
+
+}
+a{
+    color:#ef4136 ;
+}
+a:hover{
+color: white;
+}
+a:active{
+color: rgba(60, 233, 17, 0.657);
 }
 </style>
